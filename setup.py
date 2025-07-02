e3ae229bb86844fcc1d8facd39fddb340a2b5f34
fengrongman <fengrongman@lejurobot.com>
2025-07-02 14:22:21 +0800

    chore: bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rosmsg_dict_converter",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
